f45c649187fe5ef5a2e902a5700e7840a0e6e0d2
Eddie <eddie.freeman@streem.com>
2024-05-25 13:50:47 -0700

    setup env variables

---
+++ packages/web-astro/src/util/constants.ts
@@ -0,0 +1,5 @@
+// https://docs.astro.build/en/guides/environment-variables/
+export const isProd = import.meta.env.PROD;
+export const isDev = import.meta.env.DEV;
+export const baseURL = import.meta.env.BASE_URL;
+export const openaiToken = import.meta.env.OPEN_AI_TOKEN;
