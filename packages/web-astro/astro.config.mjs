2a86f81c60eaa4cda8dad06411d6176a544a4ffc
Eddie <eddie.freeman@streem.com>
2024-05-24 22:49:32 -0700

    update to latest package versions and yarn 3+

--- packages/web-astro/astro.config.mjs
+++ packages/web-astro/astro.config.mjs
@@ -2,8 +2,12 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
+const tailwindOptions = {
+  configFile: './packages/web-astro/tailwind.config.cjs'
+}
+
 export default defineConfig({
   outDir: '../../dist/packages/web-astro',
-  integrations: [react(), tailwind({ path: 'tailwind.config.cjs' })],
+  integrations: [react(), tailwind(tailwindOptions)],
   syntaxHighlight: 'prism',
 });
