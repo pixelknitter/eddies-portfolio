0dcabb776ad7291d424c731b2e237f0c83d1c326
Eddie <eddie.freeman@streem.com>
2024-05-24 22:01:05 -0700

    update configurations and linting

--- packages/web-astro/astro.config.mjs
+++ packages/web-astro/astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import solid from '@astrojs/solid-js';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   outDir: '../../dist/packages/web-astro',
-  integrations: [react(), solid(), tailwind({ path: 'tailwind.config.cjs' })],
+  integrations: [react(), tailwind({ path: 'tailwind.config.cjs' })],
   syntaxHighlight: 'prism',
 });
