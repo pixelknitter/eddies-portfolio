9d6d98d2e9cbfa26e8ba2fe4cf674440b5106131
Eddie <eddie.freeman@streem.com>
2024-05-28 19:48:25 -0700

    connect cloudflare in prep for deployment

--- packages/web-astro/astro.config.mjs
+++ packages/web-astro/astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-
+import cloudflare from "@astrojs/cloudflare";
 const tailwindOptions = {
   configFile: './packages/web-astro/tailwind.config.cjs'
 }
@@ -10,4 +10,6 @@ export default defineConfig({
   outDir: '../../dist/packages/web-astro',
   integrations: [react(), tailwind(tailwindOptions)],
   syntaxHighlight: 'prism',
+  output: "server",
+  adapter: cloudflare()
 });
