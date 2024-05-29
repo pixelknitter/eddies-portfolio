6f0deca938c88b331483271c01cf6cf586afc936
Eddie <eddie.freeman@streem.com>
2024-05-28 19:46:56 -0700

    some light clean up of types

--- packages/web-astro/src/content/config.ts
+++ packages/web-astro/src/content/config.ts
@@ -1,53 +1,66 @@
 import { z, defineCollection, reference } from 'astro:content';
 
+const ProjectSchema = z.object({
+  title: z.string(),
+  description: z.string(),
+  image: z.object({
+    url: z.string(),
+    alt: z.string(),
+  }),
+  worksImage1: z.object({
+    url: z.string(),
+    alt: z.string(),
+  }),
+  worksImage2: z.object({
+    url: z.string(),
+    alt: z.string(),
+  }),
+  platform: z.string(),
+  stack: z.string(),
+  website: z.string(),
+  github: z.string(),
+});
+
 const projects = defineCollection({
   type: 'content',
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    image: z.object({
-      url: z.string(),
-      alt: z.string(),
-    }),
-    worksImage1: z.object({
-      url: z.string(),
-      alt: z.string(),
-    }),
-    worksImage2: z.object({
-      url: z.string(),
-      alt: z.string(),
-    }),
-    platform: z.string(),
-    stack: z.string(),
-    website: z.string(),
-    github: z.string(),
+  schema: ProjectSchema,
+});
+
+export type Project = z.infer<typeof ProjectSchema>;
+
+const BlogSchema = z.object({
+  title: z.string(),
+  // Reference a single author from the `authors` collection by `id`
+  author: reference('authors'),
+  // Reference an array of related posts from the `blog` collection by `slug`
+  relatedPosts: z.array(reference('blog')),
+  blurb: z.string(),
+  tags: z.array(z.string()),
+  heroImage: z.object({
+    url: z.string(),
+    alt: z.string(),
   }),
+  draft: z.boolean(),
 });
 
 const blog = defineCollection({
   type: 'content',
-  schema: z.object({
-    title: z.string(),
-    // Reference a single author from the `authors` collection by `id`
-    author: reference('authors'),
-    // Reference an array of related posts from the `blog` collection by `slug`
-    relatedPosts: z.array(reference('blog')),
-    blurb: z.string(),
-    tags: z.array(z.string()),
-    heroImage: z.object({
-      url: z.string(),
-      alt: z.string(),
-    }),
-    draft: z.boolean(),
-  }),
+  schema: BlogSchema,
+});
+
+export type BlogPost = Omit<z.infer<typeof BlogSchema>, 'author' | 'relatedPosts'> & { author: Author, relatedPosts: string[] };
+// FIXME: get the available slugs for posts
+
+const AuthorSchema = z.object({
+  name: z.string(),
+  portfolio: z.string().url(),
 });
 
 const authors = defineCollection({
   type: 'data',
-  schema: z.object({
-    name: z.string(),
-    portfolio: z.string().url(),
-  }),
+  schema: AuthorSchema,
 });
+
+export type Author = z.infer<typeof AuthorSchema>;
 
 export const collections = { projects, blog, authors };
