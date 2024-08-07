779747d7983dc51d36e9828f629fd742c8bd4870
Eddie <eddie.freeman@streem.com>
2024-08-06 21:47:40 -0700

    dynamic titles

---
+++ packages/web-astro/src/util/functions.ts
@@ -0,0 +1,37 @@
+/**
+ * Utility functions
+ */
+
+export const sentenceCase = (str: string) => {
+  return str
+    .toLowerCase()
+    .split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+}
+
+export const readableHostname = (hostname: string): string | undefined =>  {
+  const hostArray = hostname.split('.');
+
+  let subName: string | undefined = undefined
+
+  if(hostArray.length > 2) {
+    // get the first element of the array
+    subName = hostArray.shift();
+  }
+
+  switch(hostArray[hostArray.length - 1]) {
+    case 'engineering':
+      // inject "by" in the middle and reverse the array
+      hostArray.splice(1, 0, 'by');
+      hostArray.reverse();
+      break;
+    case 'codes':
+      // add "eddie" to the front
+      hostArray.unshift('eddie');
+      break;
+    default:
+      return undefined;
+  }
+  return `${sentenceCase(hostArray.join(' '))}${subName ? ` | ${subName}` : ''}`;
+}
